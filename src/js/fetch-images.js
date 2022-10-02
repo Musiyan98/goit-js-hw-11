6e0e4b9a067effcc7b28d07dd0d01566df240e63
Musiyan98 <musiyan98@gmail.com>
2022-10-02 10:02:25 +0300

    update

---
+++ src/js/fetch-images.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export { fetchImages };
+
+const KEY = '30265959-d9883bf0204e3a3ac8cde4b49';
+const URL = 'https://pixabay.com/api';
+
+async function fetchImages(name, page) {
+  const response = await axios
+    .get(
+      `${URL}/?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
+    )
+    .then(resp => resp.data)
+    .catch(error => console.log(error));
+  return response;
+}
